c36c7f86f7a43fc13f527939a9a0792abedd6157
KDani-99 <39048573+KDani-99@users.noreply.github.com>
2022-01-11 22:38:23 +0100

    Added extension classes

---
+++ CodeManagerServer/src/CodeManagerWebApi/Utils/Extensions/JwtTokenExtensions.cs
@@ -0,0 +1,12 @@
+﻿using System.IdentityModel.Tokens.Jwt;
+
+namespace CodeManagerWebApi.Utils.Extensions
+{
+    public static class JwtTokenExtensions
+    {
+        public static string ToBase64String(this JwtSecurityToken securityToken)
+        {
+            return new JwtSecurityTokenHandler().WriteToken(securityToken);
+        }
+    }
+}
---
+++ CodeManagerServer/src/CodeManagerWebApi/Utils/Extensions/UserExtensions.cs
@@ -0,0 +1,21 @@
+﻿using CodeManagerWebApi.DataTransfer;
+using CodeManagerWebApi.Entities;
+
+namespace CodeManagerWebApi.Utils.Extensions
+{
+    public static class UserExtensions
+    {
+
+        public static User FromDto(this UserDto userDto) => new User
+        {
+            Username = userDto.Username,
+            Password = userDto.Password
+        };
+
+        public static UserDto FromUser(this User user) => new UserDto
+        {
+            Username = user.Username,
+            Password = user.Password,
+        };
+    }
+}

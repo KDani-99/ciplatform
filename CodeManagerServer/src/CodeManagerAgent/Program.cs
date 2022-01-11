d32ad04af65c803b4e1aae9262a5c194116cb544
KDani-99 <39048573+KDani-99@users.noreply.github.com>
2022-01-11 22:33:59 +0100

    Added CodeManagerAgent project

---
+++ CodeManagerServer/src/CodeManagerAgent/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace CodeManagerAgent
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}

6678f245ad10eaa5397acc3e99305f2e9f724e83
KDani-99 <39048573+KDani-99@users.noreply.github.com>
2022-01-11 22:38:30 +0100

    Added controllers

---
+++ CodeManagerServer/src/CodeManagerWebApi/Controllers/TeamsController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Threading.Tasks;
+using CodeManagerWebApi.DataTransfer;
+using CodeManagerWebApi.Entities;
+using CodeManagerWebApi.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Extensions;
+
+namespace CodeManagerWebApi.Controllers
+{
+    [ApiController]
+    [Authorize(Roles = "User,Admin")]
+    [Route("/api/teams")]
+    [Produces("application/json")]
+    public class TeamsController : ControllerBase
+    {
+        private readonly ITeamService _teamService;
+        private readonly ILogger<TeamsController> _logger;
+
+        public TeamsController(ITeamService teamService, ILogger<TeamsController> logger)
+        {
+            _teamService = teamService ?? throw new ArgumentNullException(nameof(teamService));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create([FromBody] TeamDto teamDto)
+        {
+            var user = HttpContext.Items["user"] as User;
+            await _teamService.CreateTeam(teamDto, user);
+            return StatusCode(201);
+        }
+    }
+}
---
+++ CodeManagerServer/src/CodeManagerWebApi/Controllers/UserController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Threading.Tasks;
+using CodeManagerWebApi.DataTransfer;
+using CodeManagerWebApi.Services;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.Extensions.Logging;
+
+namespace CodeManagerWebApi.Controllers
+{
+    [ApiController]
+    [Route("/api/user")]
+    [Produces("application/json")]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserService _userService;
+        private readonly ILogger<UserController> _logger;
+
+        public UserController(IUserService userService, ILogger<UserController> logger)
+        {
+            _userService = userService ?? throw new ArgumentNullException(nameof(userService));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+
+        [HttpPost, Route("register")]
+        public async Task<IActionResult> Register([FromBody] UserDto userDto)
+        {
+            await _userService.CreateUser(userDto);
+            _logger.LogInformation($"User `{userDto.Username}` created");
+
+            return StatusCode(201);
+        }
+        
+        [HttpPost, Route("login")]
+        public async Task<IActionResult> Login([FromBody] LoginDto loginDto)
+        {
+            var authTokens = await _userService.LoginAsync(loginDto, HttpContext);
+            _logger.LogInformation($"User `{loginDto.Username}` logged in @ {DateTime.Now}");
+                
+            return Ok(authTokens);
+        }
+    }
+}

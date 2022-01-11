1d9d084bcdfc4fdde73eaf8f874cee56f6e70fc9
KDani-99 <39048573+KDani-99@users.noreply.github.com>
2022-01-11 22:39:01 +0100

    Added DbContext

---
+++ CodeManagerServer/src/CodeManagerWebApi/Database/CodeManagerDbContext.cs
@@ -0,0 +1,34 @@
+﻿using CodeManagerWebApi.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace CodeManagerWebApi.Database
+{
+    public class CodeManagerDbContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Team> Teams { get; set; }
+        public DbSet<TeamMember> TeamMembers { get; set; }
+        public DbSet<Plan> Plans { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<User>()
+                .HasOne(user => user.Plan)
+                .WithOne();
+
+            modelBuilder.Entity<Team>()
+                .HasMany(team => team.Members)
+                .WithMany(teamMember => teamMember.User.Teams);
+
+            modelBuilder.Entity<Plan>()
+                .HasData(new Plan
+                {
+                    Name = "Basic",
+                    MaxCreatedTeamsPerUser = 5,
+                    MaxJoinedTeamsPerUser = 15
+                });
+        }
+        
+        
+    }
+}

47477e662f43005756acd19d55b8522938ab5cb2
KDani-99 <39048573+KDani-99@users.noreply.github.com>
2022-01-13 22:34:21 +0100

    Changed namespaces

--- CodeManagerServer/src/CodeManagerWebApi/Exceptions/BaseHttpException.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Http;
-
-namespace CodeManagerWebApi.Exceptions
-{
-    public abstract class BaseHttpException : BadHttpRequestException
-    {
-        protected BaseHttpException(string message, int statusCode) : base(message, statusCode) {}
-    }
-}

127249b61fc2a11e05f980bd874167b916d9ca1e
KDani-99 <39048573+KDani-99@users.noreply.github.com>
2022-01-13 22:36:27 +0100

    Updated server

--- CodeManagerServer/src/CodeManagerWebApi/Startup.cs
+++ CodeManagerServer/src/CodeManagerWebApi/Startup.cs
@@ -3,10 +3,13 @@ using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using CodeManagerAgentManager.Commands;
 using CodeManagerWebApi.Database;
 using CodeManagerWebApi.Entities.Configuration;
 using CodeManagerWebApi.Repositories;
 using CodeManagerWebApi.Services;
+using CodeManagerWebApi.Utils.Extensions;
+using MassTransit;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
@@ -14,7 +17,6 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using MongoDB.Driver;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
@@ -30,26 +32,38 @@ namespace CodeManagerWebApi
         {
             _configuration = configuration ?? throw new ArgumentNullException(nameof(configuration));
         }
-
-        // This method gets called by the runtime. Use this method to add services to the container.
+        
         public void ConfigureServices(IServiceCollection services)
         {
-            /*var databaseConfiguration = _configuration.GetSection("DatabaseConfiguration").Get<DatabaseConfiguration>();
-            var mongoClient = new MongoClient();*/
-
             services
                 .Configure<JwtConfiguration>(_configuration.GetSection("JwtConfiguration"))
                 .Configure<UserConfiguration>(_configuration.GetSection("UserConfiguration"))
-                //.AddSingleton(mongoClient.GetDatabase(databaseConfiguration.Database))
-                .AddDbContext<CodeManagerDbContext>(options => options.UseNpgsql())
+                .Configure<IConfiguration>(_configuration)
+                .AddDbContext<CodeManagerDbContext>(options => options.UseNpgsql(_configuration.GetValue<string>("ConnectionString")))
                 .AddSingleton<ICredentialManagerService, CredentialManagerService>()
                 .AddSingleton<ITokenService<JwtSecurityToken>, TokenService>()
-                .AddSingleton<IUserRepository, UserRepository>()
-                .AddSingleton<IUserService, UserService>()
-                .AddSingleton<ITeamRepository, TeamRepository>()
-                .AddSingleton<ITeamService, TeamService>()
+                .AddScoped<IUserRepository, UserRepository>()
+                .AddScoped<IUserService, UserService>()
+                .AddScoped<ITeamRepository, TeamRepository>()
+                .AddScoped<ITeamService, TeamService>()
+                .AddScoped<IPlanRepository, PlanRepository>()
+                .AddScoped<IPlanService, PlanService>()
                 .AddAntiforgery()
-                
+                .AddMassTransit(options =>
+                {
+                    var bus = Bus.Factory.CreateUsingRabbitMq(config =>
+                    {
+                        config.Host("localhost", "/",
+                            h =>
+                            {
+                                h.Username("guest");
+                                h.Password("guest");
+                            });
+                    });
+                    options.AddRequestClient<StartJobCommand>();
+                    
+                    bus.Start();
+                })
                 .AddControllers().Services
                 .AddSwaggerGen(c =>
                 {
@@ -60,42 +74,12 @@ namespace CodeManagerWebApi
             services.AddAuthentication("JwtAuthToken")
                 .AddJwtBearer(opts => opts.MapInboundClaims = false)
                 .AddScheme<JwtAuthenticationTokenSchemeOptions, JwtAuthenticationHandler>("JwtAuthToken",null);
-            
-            // Configure authentication (TODO: extension class)
-           /* services
-                .AddAuthentication(options =>
-                {
-                    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                    options.DefaultScheme = JwtBearerDefaults.AuthenticationScheme;
-                    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
-                })
-                .AddJwtBearer(options =>
-                {
-                    var jwtConfiguration = _configuration.GetSection("JwtConfiguration").Get<JwtConfiguration>();
-                    options.SaveToken = true;
-                    options.TokenValidationParameters = new TokenValidationParameters
-                    {
-                        ValidateIssuerSigningKey = true,
-                        IssuerSigningKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(jwtConfiguration.Secret)),
-                        ValidateIssuer = false,
-                        ValidateAudience = false,
-                        ValidateLifetime = true,
-                        RequireExpirationTime = true
-                    };
-                    options.
-                });*/
         }
-
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        
         public void Configure(
             IApplicationBuilder app,
-            IWebHostEnvironment env,
-            IUserRepository userRepository,
-            ICredentialManagerService credentialManagerService,
-            ILogger<BootstrapService> logger)
+            IWebHostEnvironment env)
         {
-            Bootstrap(userRepository, credentialManagerService, logger);
-
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
@@ -104,22 +88,13 @@ namespace CodeManagerWebApi
             }
 
             app
+                .UseExceptionHandler("/error")
                 .UseHttpsRedirection()
                 .UseRouting()
                 .UseAuthentication()
                 .UseAuthorization()
                 .UseEndpoints(endpoints => { endpoints.MapControllers(); })
                 .UseHealthChecks("/api/health");
-        }
-
-        private void Bootstrap(
-            IUserRepository userRepository,
-            ICredentialManagerService credentialManagerService,
-            ILogger<BootstrapService> logger)
-        {
-            var accountConfiguration = _configuration.GetSection("ServiceAccountConfiguration").Get<ServiceAccountConfiguration>();
-
-            new BootstrapService(userRepository, credentialManagerService, logger).Invoke(accountConfiguration).Wait();
         }
     }
 }

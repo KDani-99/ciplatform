889c753a065cf07e90cd7f5f6bca0cdf8fb5f5ba
KDani-99 <39048573+KDani-99@users.noreply.github.com>
2022-01-13 22:36:08 +0100

    Added PlanDto

---
+++ CodeManagerServer/src/CodeManagerWebApi/DataTransfer/PlanDto.cs
@@ -0,0 +1,9 @@
+﻿namespace CodeManagerWebApi.DataTransfer
+{
+    public class PlanDto
+    {
+        public string Name { get; set; }
+        public int MaxCreatedTeamsPerUser { get; set; }
+        public int MaxJoinedTeamsPerUser { get; set; }
+    }
+}

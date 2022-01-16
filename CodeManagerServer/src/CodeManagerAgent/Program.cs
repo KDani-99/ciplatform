a107cff8d1054e937ec9596ba244088fb7c956f1
KDani-99 <39048573+KDani-99@users.noreply.github.com>
2022-01-16 10:43:18 +0100

    Updated entry point with conditional (platform specific) service registration

--- CodeManagerServer/src/CodeManagerAgent/Program.cs
+++ CodeManagerServer/src/CodeManagerAgent/Program.cs
@@ -1,7 +1,14 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Threading.Tasks;
+using CodeManager.Data.Configuration;
+using CodeManagerAgent.Configuration;
+using CodeManagerAgent.Factories;
+using CodeManagerAgent.Services;
+using GreenPipes.Agents;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
@@ -16,6 +23,44 @@ namespace CodeManagerAgent
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
-                .ConfigureServices((hostContext, services) => { services.AddHostedService<Worker>(); });
+                .ConfigureServices((hostContext, services) =>
+                {
+                    var agentConfiguration = hostContext.Configuration.GetValue<AgentConfiguration>("AgentConfiguration");
+                    VerifyContext(agentConfiguration);
+
+                    switch (agentConfiguration.Context)
+                    {
+                        case JobContext.Linux:
+                            services.AddScoped<IJobHandlerServiceFactory, LinuxJobHandlerServiceFactory>();
+                            break;
+                        case JobContext.Windows:
+                            services.AddScoped<IJobHandlerServiceFactory, WindowsJobHandlerServiceFactory>();
+                            break;
+                        case JobContext.Docker:
+                            services.AddScoped<IJobHandlerServiceFactory, DockerJobHandlerServiceFactory>();
+                            break;
+                        default:
+                            throw new ArgumentOutOfRangeException(
+                                "The agent context must be one of the following types: 'Linux', 'Windows', 'Docker'");
+                    }
+                    
+                    services.AddHostedService<Worker>();
+                    services.AddSingleton<IAgentService, AgentService>();
+                    //services.AddScoped<IJobHandlerServiceFactory, JobHandlerServiceFactory>();
+                });
+
+        private static void VerifyContext(AgentConfiguration agentConfiguration)
+        {
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Linux) && agentConfiguration.Context != JobContext.Linux && agentConfiguration.Context != JobContext.Docker)
+            {
+                throw new InvalidOperationException("The agent context may only be either 'Docker' or 'Linux' on this machine");
+            }
+
+            if (RuntimeInformation.IsOSPlatform(OSPlatform.Windows) &&
+                agentConfiguration.Context != JobContext.Windows && agentConfiguration.Context != JobContext.Docker)
+            {
+                throw new InvalidOperationException("The agent context may only be either 'Docker' or 'Windows' on this machine");
+            }
+        }
     }
 }

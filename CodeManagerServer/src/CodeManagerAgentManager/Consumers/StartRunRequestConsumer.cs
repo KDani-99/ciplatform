5a67ef0595735fa6c549427580b8afe621365936
KDani-99 <39048573+KDani-99@users.noreply.github.com>
2022-01-16 10:41:00 +0100

    Added StartRunRequestConsumer

---
+++ CodeManagerServer/src/CodeManagerAgentManager/Consumers/StartRunRequestConsumer.cs
@@ -0,0 +1,19 @@
+﻿using System.Threading.Tasks;
+using CodeManager.Data.Messaging;
+using MassTransit;
+
+namespace CodeManagerAgentManager.Consumers
+{
+    public class StartRunRequestConsumer : IConsumer<StartRunRequest>
+    {
+        
+        
+        public async Task Consume(ConsumeContext<StartRunRequest> context)
+        {
+            // TODO: request agents
+            
+
+            await context.RespondAsync(); // TODO: responde with status => started/agent not available
+        }
+    }
+}

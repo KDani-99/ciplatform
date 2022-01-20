7fb0006dbeeaf96737f8036aec58ced055a11ed9
KDani-99 <39048573+KDani-99@users.noreply.github.com>
2022-01-20 23:19:07 +0100

    Added new consumers

---
+++ CodeManagerServer/src/CodeManagerAgentManager/Consumers/QueueRunCommandConsumer.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Threading.Tasks;
+using CodeManager.Data.Commands;
+using CodeManagerAgentManager.Services;
+using MassTransit;
+using Microsoft.Extensions.Logging;
+
+namespace CodeManagerAgentManager.Consumers
+{
+    public class QueueRunCommandConsumer : IConsumer<QueueRunCommand>
+    {
+        private readonly IRunService<QueueRunCommand> _runService;
+        private readonly ILogger<QueueRunCommandConsumer> _logger;
+
+        public QueueRunCommandConsumer(IRunService<QueueRunCommand> runService, ILogger<QueueRunCommandConsumer> logger)
+        {
+            _runService = runService ?? throw new ArgumentNullException(nameof(runService));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+        
+        public async Task Consume(ConsumeContext<QueueRunCommand> context)
+        {
+            try
+            {
+                var runId = await _runService.QueueAsync(context.Message);
+
+                await context.RespondAsync(new QueueRunCommandResponse
+                {
+                    RunId = runId
+                });
+            }
+            catch (Exception exception)
+            {
+                // TODO: send error
+                _logger.LogError($"Failed to consume `{nameof(QueueRunCommand)}`. Error: {exception.StackTrace}");
+            }
+        }
+    }
+}
---
+++ CodeManagerServer/src/CodeManagerAgentManager/Consumers/StepResultEventConsumer.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Threading.Tasks;
+using Automatonymous;
+using CodeManager.Data.Configuration;
+using CodeManager.Data.Events;
+using CodeManager.Data.JsonWebTokens;
+using CodeManager.Data.Repositories;
+using CodeManagerAgentManager.Exceptions;
+using CodeManagerAgentManager.Services;
+using MassTransit;
+using Microsoft.Extensions.Logging;
+
+namespace CodeManagerAgentManager.Consumers
+{
+    public class StepResultEventConsumer : IConsumer<StepResultEvent>
+    {
+        private readonly IRunRepository _runRepository;
+        private readonly ITokenService<JwtSecurityToken> _tokenService;
+        private readonly ILogger<StepResultEventConsumer> _logger;
+
+        public StepResultEventConsumer(IRunRepository runRepository, ITokenService<JwtSecurityToken> tokenService, ILogger<StepResultEventConsumer> logger)
+        {
+            _runRepository = runRepository ?? throw new ArgumentNullException(nameof(runRepository));
+            _tokenService = tokenService ?? throw new ArgumentNullException(nameof(tokenService));
+            _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        }
+        
+        public async Task Consume(ConsumeContext<StepResultEvent> context)
+        {
+            try
+            {
+                var token = await _tokenService.VerifyJobTokenAsync(context.Message.Token);
+                
+                var runId = long.Parse(token.Claims.First(claim => claim.Type == CustomJwtRegisteredClaimNames.RunId).Value);
+                var jobId = long.Parse(token.Claims.First(claim => claim.Type == CustomJwtRegisteredClaimNames.JobId)
+                    .Value);
+                
+                var run = await _runRepository.GetAsync(runId) ?? throw new RunDoesNotExistException();
+                var job = run.Jobs.First(item => item.Id == jobId);
+
+                var step = job.Steps[context.Message.StepIndex];
+                step.State = context.Message.State;
+
+                if (step.State is States.Successful or States.Failed)
+                {
+                    step.FinishedDateTime = DateTime.Now;
+                }
+                
+                if (step.State == States.Failed)
+                {
+                    job.State = States.Failed;
+                }
+
+                if (step.State == States.Successful && context.Message.StepIndex == job.Steps.Count - 1)
+                {
+                    job.State = States.Successful;
+                }
+                
+                await _runRepository.UpdateAsync(run);
+                // TODO: broadcast it to the client
+                
+            }
+            catch (Exception exception)
+            {
+                _logger.LogError($"Failed to consume `{nameof(StepResultEvent)}`. Error: {exception.StackTrace}");
+            }
+        }
+    }
+}

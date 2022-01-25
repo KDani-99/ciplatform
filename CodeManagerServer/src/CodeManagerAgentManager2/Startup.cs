5ca8aa12996113c97a38d57ee848b20a100f824d
KDani-99 <39048573+KDani-99@users.noreply.github.com>
2022-01-25 23:04:47 +0100

    [Obsolote]

---
+++ CodeManagerServer/src/CodeManagerAgentManager2/Startup.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Builder;
+
+namespace CodeManagerAgentManager
+{
+   public class Startup
+    {
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+                app.UseDeveloperExceptionPage();
+            else
+                app.UseHsts();
+
+            app.UseRouting();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Hello World!");
+                });
+            });
+        }
+    }
+}

16371977d84282932862b5aa5079ae47fb01658b
KDani-99 <39048573+KDani-99@users.noreply.github.com>
2022-01-30 22:37:02 +0100

    Refactored validation and added FluentValidation

---
+++ CodeManagerServer/src/CodeManagerWebApi/DataTransfer/CreateTeamDto.cs
@@ -0,0 +1,27 @@
+﻿namespace CodeManagerWebApi.DataTransfer
+{
+    public class TeamDto
+    {
+        public string Name { get; set; }
+        public string Image { get; set; }
+    }
+    
+    public class UserDtoValidator : AbstractValidator<UserDto> {
+        public UserDtoValidator() {
+            RuleFor(x => x.Username)
+                .NotNull()
+                .Matches("(^[a-zA-Z0-9]{1}([a-zA-Z0-9-_]{2,48})[a-zA-Z0-9]{1}$)")
+                .WithMessage("Field `username` must be between 4 and 50 characters.");
+            RuleFor(x => x.Name)
+                .Length(1, 75)
+                .WithMessage("Field `name` must be between 1 and 75 characters.");
+            RuleFor(x => x.Email)
+                .EmailAddress()
+                .WithMessage("Field `email` is invalid.");
+            RuleFor(x => x.Password)
+                .NotNull()
+                .Length(8, 255)
+                .WithMessage("Field `password` is invalid.");
+        }
+    }
+}
--- CodeManagerServer/src/CodeManagerWebApi/DataTransfer/TeamDto.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace CodeManagerWebApi.DataTransfer
-{
-    public class TeamDto
-    {
-        public string Name { get; set; }
-        public string Image { get; set; }
-    }
-}

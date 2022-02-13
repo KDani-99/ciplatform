4b10395581e0b0cef9fa95edc1575a5de9b7e501
KDani-99 <39048573+KDani-99@users.noreply.github.com>
2022-02-13 21:25:59 +0100

    Removed unused classes

---
+++ CodeManagerServer/src/CodeManager.Data/Events/ProcessedStepResultEvent.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using CodeManager.Data.Configuration;
+
+namespace CodeManager.Data.Entities
+{
+    public class ProcessedStepResult
+    {
+        public long JobId { get; set; }
+        public long StepId { get; set; }
+        public States State { get; set; }
+        public DateTime DateTime { get; set; }
+    }
+}
